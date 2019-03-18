f18c54dbbf2c72fcf8592c0cbf8ea128b7b41818
Kiagus Arief Adriansyah <arief@alodokter.com>
2019-03-18 13:09:02 +0700

    make name proper

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --rm --name es -v es_data:/data -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" kadriansyah/es
+docker run -d --rm --name es -v es_data:/data -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" kadriansyah/centos_07_elasticsearch:v1
